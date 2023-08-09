97fbe0eb7a0c8badb94e657a0e609c97ae02ad6a
rashmi211 <salunkers213@gmail.com>
2023-08-09 20:27:40 +0530

    Added new test

---
+++ src/test/java/test/NewTestAdded.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTestAdded {
+	
+	@Test
+	public void newtest() {
+		int a=10; int b=45;
+		System.out.println(a+b);
+	}
+	
+		
+
+}
